4c52332afadec0110181bf6f7c5ae0db05aa878e
Daniel Esteban Martinez Herrera <danielestebanmat@gmail.com>
2022-10-11 15:34:37 -0500

    Mi primer Commit

---
+++ src/Flecha.java
@@ -0,0 +1,45 @@
+
+public class Flecha {
+	
+	int longitud;
+	String color;
+	
+	public Flecha() {
+		
+		longitud = 18;
+		color = "negro";
+		
+	}
+	
+	public Flecha( int longitud, String color) {
+		
+		this.longitud = longitud;
+		this.color = color;
+		
+	}
+	
+	public void imprimirEspacio() {
+		
+		System.out.println();
+		
+	}
+	
+	public void imprime( String simbolo ) {
+		
+		if (color.equals("negro")) {
+			System.out.print( simbolo);
+		} else {
+			System.err.print( simbolo );
+		}
+		
+	}
+
+	public void construirFlecha() {
+		for (int i = 0; i < longitud; i++) {
+			imprime("-");
+		}
+		imprime(">");		
+	}
+
+	
+}
